2809d7814ee177d18d8c5a820093aa3aef028822
Jiahao Chen <horychen@qq.com>
2020-11-19 15:27:24 +0800

    1.0.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="emachinery",
-    version="1.0.0",
+    version="1.0.2",
     description="A package for electric machinery analysis.",
     long_description=README,
     long_description_content_type="text/markdown",
