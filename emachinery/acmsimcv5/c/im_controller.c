9995a0bb2a8f1be7bbedfb9aee3023a90a2696c1
Jiahao Chen <horychen@qq.com>
2021-01-08 01:10:11 +0800

    [acmsimc] 开环磁链观测反馈，调节 lambda_inv（eta的增益）和 kz 有帮助哦！

--- emachinery/acmsimcv5/c/im_controller.c
+++ emachinery/acmsimcv5/c/im_controller.c
@@ -8,13 +8,14 @@ struct ControllerForExperiment CTRL;
 void CTRL_init(){
 
     // struct Marino2005
-    marino.kz         = 700.0; // zd, zq
+    marino.kz         = 2*700.0; // zd, zq
 
     marino.k_omega    = 88*60.0; // 6000  // e_omega // 增大这个可以消除稳态转速波形中的正弦扰动（源自q轴电流给定波形中的正弦扰动，注意实际的q轴电流里面是没有正弦扰动的）
     marino.kappa      = 24;      //0.05;  // e_omega // 增大这个意义不大，转速控制误差基本上已经是零了，所以kappa取0.05和24没有啥区别。
 
-    marino.gamma_inv  = 1e-4 * 180*CTRL.Js_inv; // TL    磁链反馈为实际值时，这两个增益取再大都没有意义。
-    marino.lambda_inv = 1e-4 * 6000.0;          // omega 磁链反馈为实际值时，这两个增益取再大都没有意义。
+    marino.lambda_inv = 1e-1 * 6000.0;          // omega 磁链反馈为实际值时，这两个增益取再大都没有意义。
+
+    marino.gamma_inv  = 0*1e-4 * 180*CTRL.Js_inv; // TL    磁链反馈为实际值时，这两个增益取再大都没有意义。
     marino.delta_inv  = 0*75.0; // alpha 要求磁链幅值时变
 
     marino.xTL_Max = 8.0;
@@ -327,8 +328,8 @@ void controller_marino2005(){
     // flux feedback
     marino.psi_Dmu = holtz.psi_D2;
     marino.psi_Qmu = holtz.psi_Q2;
-    // marino.psi_Dmu = holtz.psi_D2_ode1;
-    // marino.psi_Qmu = holtz.psi_Q2_ode1;
+    marino.psi_Dmu = holtz.psi_D2_ode1;
+    marino.psi_Qmu = holtz.psi_Q2_ode1;
     // flux error quantities
     marino.e_psi_Dmu = marino.psi_Dmu - CTRL.psi_cmd;
     marino.e_psi_Qmu = marino.psi_Qmu - 0.0;
@@ -340,7 +341,7 @@ void controller_marino2005(){
     CTRL.alpha   = marino.xAlpha;
     CTRL.TLoad   = marino.xTL;
 
-    // debug
+    // 磁场可测 debug
     CTRL.theta_D = ACM.theta_M;
     CTRL.omg__fb = im.omg_elec;
     CTRL.alpha   = ACM.alpha;
